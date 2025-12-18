9a3ebee0ba0d1f27d35e6d16a5d8c9f7ea099e42
chaitanya <vamsi.blaze1@gmail.com>
2025-12-18 18:42:36 +0530

    deleted

--- 10-sg/data.tf
+++
@@ -1,4 +0,0 @@
-//this is used to query data this vpc id is already present in parameter store
-data "aws_ssm_parameter" "vpc_id" {
-  name = "/${var.project_name}/${var.environment}/vpc_id"
-}
--- 10-sg/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  vpc_id             = data.aws_ssm_parameter.vpc_id.value
-  common_name_prefix = "${var.project_name} - ${var.environment}" #roboshop - dev
-}
