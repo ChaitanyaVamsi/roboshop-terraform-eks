b1957904b04fd1c7999a7c8025197cc3ee96ecb6
chaitanya <vamsi.blaze1@gmail.com>
2026-01-19 18:24:30 +0530

    updated

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -1,17 +1,15 @@
 resource "aws_lb" "ingress_alb" {
-  name               = "${local.common_name_suffix}-ingress-alb" # roboshop-dev-backend-alb
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [local.ingress_alb_sg_id]
-  # it should be private subnet ids
-  subnets            = local.public_subnet_ids
+  name                       = "${local.common_name_suffix}-ingress-alb" # roboshop-dev-backend-alb
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [local.ingress_alb_sg_id]
+  subnets                    = local.public_subnet_ids # it should be private subnet ids
+  enable_deletion_protection = false                   # prevents accidental deletion from UI
 
-  enable_deletion_protection = false # prevents accidental deletion from UI
-
-  tags = merge (
+  tags = merge(
     local.common_tags,
     {
-        Name = "${local.common_name_suffix}-ingress-alb"
+      Name = "${local.common_name_suffix}-ingress-alb"
     }
   )
 }
@@ -35,9 +33,9 @@ resource "aws_lb_listener" "ingress_alb" {
 }
 
 resource "aws_route53_record" "ingress_alb" {
-  zone_id = var.zone_id
-  name    = "*.${var.domain_name}" # *.daws86s.fun
-  type    = "A"
+  zone_id         = var.zone_id
+  name            = "*.${var.domain_name}" # *.daws86s.fun
+  type            = "A"
   allow_overwrite = true
 
   alias {
@@ -45,5 +43,41 @@ resource "aws_route53_record" "ingress_alb" {
     name                   = aws_lb.ingress_alb.dns_name
     zone_id                = aws_lb.ingress_alb.zone_id
     evaluate_target_health = true
+  }
+}
+
+resource "aws_lb_target_group" "frontend" {
+  name                 = "${local.common_name_suffix}-frontend"
+  port                 = 8080
+  protocol             = "HTTP"
+  target_type          = "ip"
+  vpc_id               = local.vpc_id
+  deregistration_delay = 60 # waiting period before deleting the instance
+
+  health_check {
+    healthy_threshold   = 2
+    interval            = 10
+    matcher             = "200-299"
+    path                = "/"
+    port                = 8080
+    protocol            = "HTTP"
+    timeout             = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "frontend" {
+  listener_arn = aws_lb_listener.ingress_alb.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.environment}.${var.domain_name}"] # dev.cloudops.store
+    }
   }
 }
